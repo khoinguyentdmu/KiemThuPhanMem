02ec026d041abc938e6ae9b410b6befe7efc2e43
Khoi <khoi.nguyen.tdmu@gmail.com>
2018-10-15 13:11:13 +0700

    first commit

---
+++ Calculator/TestNUnit/Sample.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNUnit
+{
+    [TestFixture]
+    class Sample
+    {
+        [Test]
+        public void HelloWolrd()
+        {
+            Assert.AreEqual("HelloWorld", "HelloWorld");
+        }
+    }
+}
