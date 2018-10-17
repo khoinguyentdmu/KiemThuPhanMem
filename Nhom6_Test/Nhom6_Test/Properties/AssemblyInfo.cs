0c6b9674847ab24a5e12a0f8613f69a46d1963a0
Khoi <khoi.nguyen.tdmu@gmail.com>
2018-10-17 15:48:13 +0700

    Merge code

--- Nhom6_Test/Nhom6_Test/Properties/AssemblyInfo.cs
+++ Nhom6_Test/Nhom6_Test/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9da4ba56-c5ad-4140-8047-705e080737df")]
+[assembly: Guid("262ce414-a791-4fc7-a63c-58fd69c9576e")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Nhom6_Test/Nhom6_Test/Test_GiaiHPTbacnhat3an.cs
+++ Nhom6_Test/Nhom6_Test/Test_GiaiHPTbacnhat3an.cs
@@ -1,5 +1,5 @@
-﻿using Calculator;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using Calculator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
